1114655f20ff9447cafb814b4efd0b2332730247
ArthurR85 <arofda-falcon@gmx.de>
2024-11-30 19:04:29 +0100

    Getter und Setter in Bestellung.java hinzugefügt

--- src/main/java/vorlage/Bestellung.java
+++ src/main/java/vorlage/Bestellung.java
@@ -12,27 +12,47 @@ public class Bestellung {
     private Kellner kellner;
     private double gesamtbetrag;
 
-    // Konstruktor, Getter und Setter
+    public Bestellung(int bestellnummer, Date bestelldatum, Kunde kunde, Kellner kellner) {
+        this.bestellnummer = bestellnummer;
+        this.bestelldatum = bestelldatum;
+        this.gerichte = new ArrayList<>();
+        this.kunde = kunde;
+        this.kellner = kellner;
+    }
+
+    public int getBestellnummer() {
+        return bestellnummer;
+    }
+
+    public Date getBestelldatum() {
+        return bestelldatum;
+    }
+
+    public List<Gericht> getGerichte() {
+        return gerichte;
+    }
+
+    public Kunde getKunde() {
+        return kunde;
+    }
+
+    public Kellner getKellner() {
+        return kellner;
+    }
 
     public double getGesamtbetrag() {
- 
-		return gesamtbetrag;
-	}
+        return gesamtbetrag;
+    }
 
-	public void gerichtHinzufuegen(Gericht gericht) {
-  
+    public void gerichtHinzufuegen(Gericht gericht) {
+        gerichte.add(gericht);
+        gesamtbetragBerechnen();
     }
 
     public void gesamtbetragBerechnen() {
-   
+        gesamtbetrag = 0;
+        for (Gericht gericht : gerichte) {
+            gesamtbetrag += gericht.getPreis();
+        }
     }
-
-	public Bestellung(int bestellnummer, Date bestelldatum, Kunde kunde, Kellner kellner) {
-		super();
-		this.bestellnummer = bestellnummer;
-		this.bestelldatum = bestelldatum;
-		this.gerichte = new ArrayList<Gericht>();
-		this.kunde = kunde;
-		this.kellner = kellner;
-	}
 }
